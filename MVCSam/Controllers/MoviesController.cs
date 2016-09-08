d5330a5c4b36db8a1b7714f66cb64ddeed9ad663
xiaojunp <xiaojunp@china-qizhi.com>
2016-09-08 14:02:55 +0800

    Solution - Section 2 Exercise - Display the list of customers and movies

--- MVCSam/Controllers/MoviesController.cs
+++ MVCSam/Controllers/MoviesController.cs
@@ -8,7 +8,26 @@ namespace MVCSam.Controllers
     public class MoviesController : Controller
     {
         private string sortBy;
-        // GET: Movies/Random
+
+        // movies/index
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+            
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+
+        }
+        
+        //// GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie {Name = "Shrek!"};
@@ -32,15 +51,15 @@ namespace MVCSam.Controllers
             return Content("movieId = " + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            this.sortBy = sortBy;
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "name";
-            return Content(string.Format("pageid={0}&sortby={1}", pageIndex, sortBy));
-        }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    this.sortBy = sortBy;
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "name";
+        //    return Content(string.Format("pageid={0}&sortby={1}", pageIndex, sortBy));
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{2})}")]
         public ActionResult ByReleasedDate(int year, int month)
