b5890322c9c137fe8d1e5d228936227a310b0cd7
xiaojunp <xiaojunp@china-qizhi.com>
2016-09-23 07:41:15 +0800

    Section4 Building Forms

--- MVCSam/Controllers/CustomersController.cs
+++ MVCSam/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using MVCSam.Models;
+using MVCSam.ViewModels;
 
 namespace MVCSam.Controllers
 {
@@ -23,7 +24,54 @@ namespace MVCSam.Controllers
         {
             //var customers = GetCustomers();
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+           
             return View(customers);
+        }
+
+
+        public ActionResult New()
+        {
+            var membershiptype = _context.MembershipTypes.ToList();
+            var newCustomerViewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershiptype
+            };
+            return View("CustomerForm",newCustomerViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var customerViewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            }; 
+
+            return View("CustomerForm", customerViewModel);
         }
 
         public ActionResult Details(int id)
---
+++ MVCSam/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCSam.Models;
+
+namespace MVCSam.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
